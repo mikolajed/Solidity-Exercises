4aa89ac6cffb9cf20c632b398d136a34c337c331
mikolajed <mikolajedjed@gmail.com>
2026-07-03 23:40:51 +0200

    feat: solve storage arrays exercises

--- Stack/src/Stack.sol
+++ Stack/src/Stack.sol
@@ -21,5 +21,27 @@ contract Stack {
         stack = _stack;
     }
 
-    // your code here
+    function push(uint256 value) public {
+        stack.push(value);
+    }
+
+    function peek() public view returns (uint256) {
+        require(stack.length > 0, "Stack is empty");
+        return stack[stack.length - 1];
+    }
+
+    function pop() public returns (uint256) {
+        require(stack.length > 0, "Stack is empty");
+        uint256 lastVal = stack[stack.length - 1];
+        stack.pop();
+        return lastVal;
+    }
+
+    function size() public view returns (uint256) {
+        return stack.length;
+    }
+
+    function getStack() public view returns (uint256[] memory) {
+        return stack;
+    }
 }
